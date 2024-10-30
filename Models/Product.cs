bf41ea390cf25e55c334575f894c50713d3e6e24
Juand7M <trabajosfcecep4@gmail.com>
2024-10-30 01:38:05 -0500

    Modelos y configuración del DbContext

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        // [Key] antacion si la propiedad se llama id, ejemplo productid
+        public int Id { get; set; } //sera la llave primaria
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6,2)")]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; } //sera la llave foranea
+        public Category Category { get; set; } // propiedad de navegacion
+    }
+}
