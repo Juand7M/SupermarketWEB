729d10749c00053faafd4019fda58cc701527d22
Juand7M <trabajosfcecep4@gmail.com>
2024-10-30 23:37:52 -0500

    Creacion de las clases Customers y Pay mode en models y añadirlos en la clase supermarketContext

---
+++ Models/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace SupermarketWEB.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string docNumer { get; set; }
+		public string firstName { get; set; }
+		public string lastName { get; set; }
+		public string Phone { get; set; }
+		public string Birthday { get; set; }
+		public string Address { get; set; }
+		public string Email { get; set; }
+	}
+}
---
+++ Models/PayMode.cs
@@ -0,0 +1,8 @@
+﻿namespace SupermarketWEB.Models
+{
+	public class PayMode
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
