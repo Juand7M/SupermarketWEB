83fad24a03b0b38a59a3c798a71cf5dfed0bc4e9
Juand7M <trabajosfcecep4@gmail.com>
2024-10-31 00:05:55 -0500

    Index Customers

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -3,7 +3,7 @@
 	public class Customer
 	{
 		public int Id { get; set; }
-		public string docNumer { get; set; }
+		public string docNumber { get; set; }
 		public string firstName { get; set; }
 		public string lastName { get; set; }
 		public string Phone { get; set; }
