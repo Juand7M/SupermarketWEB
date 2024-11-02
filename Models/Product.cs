8ca55df74474a2671830687e6d05ed52f524ade4
Juand7M <trabajosfcecep4@gmail.com>
2024-11-02 11:01:17 -0500

    Create Products

--- Models/Product.cs
+++ Models/Product.cs
@@ -12,6 +12,6 @@ namespace SupermarketWEB.Models
         public decimal Price { get; set; }
         public int Stock { get; set; }
         public int CategoryId { get; set; } //sera la llave foranea
-        public Category Category { get; set; } // propiedad de navegacion
+        public Category? Category { get; set; } // propiedad de navegacion
     }
 }
