e630587691d0f3de6ca6fac916014c170f900e06
Juand7M <trabajosfcecep4@gmail.com>
2024-11-09 14:03:34 -0500

    implementacion de la tabla user

--- Pages/Account/Login.cshtml.cs
+++ Pages/Account/Login.cshtml.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore; // Asegúrate de tener esta línea
+using SupermarketWEB.Data;
 using SupermarketWEB.Models;
 using System.Security.Claims;
 
@@ -8,29 +10,43 @@ namespace SupermarketWEB.Pages.Account
 {
     public class LoginModel : PageModel
     {
-        [BindProperty]
-        public User User { get; set; }
-        public void OnGet()
+        private readonly SupermarketContext _context;
+
+        public LoginModel(SupermarketContext context)
         {
+            _context = context;
         }
+
+        [BindProperty]
+        public User User { get; set; } = default!;
 
         public async Task<IActionResult> OnPostAsync()
         {
             if (!ModelState.IsValid) return Page();
 
-            if (User.Email == "juandavid@gmail.com" && User.Password == "12345")
+
+            var user = await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == User.Email && u.Password == User.Password);
+
+            if (user != null)
             {
+
                 var claims = new List<Claim>
                 {
-                    new Claim(ClaimTypes.Name,"admin"),
-                    new Claim(ClaimTypes.Email,User.Email),
+                    new Claim(ClaimTypes.Email, user.Email),
+                    new Claim(ClaimTypes.Email, user.Email),
                 };
 
                 var identity = new ClaimsIdentity(claims, "MyCookieAuth");
                 ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(identity);
+
+
                 await HttpContext.SignInAsync("MyCookieAuth", claimsPrincipal);
-                return RedirectToPage("/index");
+                return RedirectToPage("/Index");
             }
+
+
+            ModelState.AddModelError(string.Empty, "Credenciales inválidas.");
             return Page();
         }
     }
